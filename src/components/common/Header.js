a851754d1d175ba75c51ace72e70e51cda28c090
yatishpro <yatishpro@gmail.com>
2019-05-14 23:04:28 +0530

    first commit

---
+++ src/components/common/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.heading}</Text>
+        </View>
+    );
+};
+
+export { Header };
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#D382FF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20,
+        color: '#ffffff'
+    }
+};
+
